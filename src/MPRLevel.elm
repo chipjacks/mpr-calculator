58a9d8a46d2057cc5caa5f7c8a066807534e3205
Chip Jackson <chipjacks@gmail.com>
2020-02-17 08:27:03 -0800

    Elm format

--- src/MPRLevel.elm
+++ src/MPRLevel.elm
@@ -1,165 +1,194 @@
-module MPRLevel exposing (lookup, equivalentRaceTimes, trainingPaces, RunnerType(..), distanceList, paceList, timeStrToHrsMinsSecs, timeToSeconds, stripTimeStr)
+module MPRLevel exposing (RunnerType(..), distanceList, equivalentRaceTimes, lookup, paceList, stripTimeStr, timeStrToHrsMinsSecs, timeToSeconds, trainingPaces)
 
-import MPRData
-import Json.Decode exposing (decodeString, dict, array, list, string)
-import Dict exposing (Dict)
 import Array exposing (Array)
+import Dict exposing (Dict)
+import Json.Decode exposing (array, decodeString, dict, list, string)
+import MPRData
 import Result
+
 
 
 -- Load tables
 
+
 distanceList : List String
-distanceList = ["5k", "8k", "5 mile", "10k", "15k", "10 mile", "20k", "Half Marathon", "25k", "30k", "Marathon"]
+distanceList =
+    [ "5k", "8k", "5 mile", "10k", "15k", "10 mile", "20k", "Half Marathon", "25k", "30k", "Marathon" ]
+
 
 paceList : List String
-paceList = ["Easy", "Moderate", "Steady State", "Brisk", "Aerobic Threshold", "Lactate Threshold", "Groove", "VO2 Max", "Fast"]
+paceList =
+    [ "Easy", "Moderate", "Steady State", "Brisk", "Aerobic Threshold", "Lactate Threshold", "Groove", "VO2 Max", "Fast" ]
 
 
 equivalentRaceTimesTable : RunnerType -> Dict String (Array String)
 equivalentRaceTimesTable runnerType =
-  let
-      json =
-        case runnerType of
-          Neutral ->
-            MPRData.neutralRace
+    let
+        json =
+            case runnerType of
+                Neutral ->
+                    MPRData.neutralRace
 
-          Aerobic ->
-            MPRData.aerobicRace
+                Aerobic ->
+                    MPRData.aerobicRace
 
-          Speed ->
-            MPRData.speedRace
-  in
+                Speed ->
+                    MPRData.speedRace
+    in
     decodeString (dict (array string)) json |> Result.withDefault Dict.empty
 
 
-trainingPacesTable : RunnerType -> Array (Array (String, String))
+trainingPacesTable : RunnerType -> Array (Array ( String, String ))
 trainingPacesTable runnerType =
-  let
-      json =
-        case runnerType of
-          Neutral ->
-            MPRData.neutralTraining
+    let
+        json =
+            case runnerType of
+                Neutral ->
+                    MPRData.neutralTraining
 
-          Aerobic ->
-            MPRData.aerobicTraining
+                Aerobic ->
+                    MPRData.aerobicTraining
 
-          Speed ->
-            MPRData.speedTraining
-  in
+                Speed ->
+                    MPRData.speedTraining
+    in
     decodeString (array (array (list string))) json
-      |> Result.withDefault Array.empty
-      |> Array.map (\a -> Array.map (\t -> toTuple t |> Maybe.withDefault ("", "")) a )
+        |> Result.withDefault Array.empty
+        |> Array.map (\a -> Array.map (\t -> toTuple t |> Maybe.withDefault ( "", "" )) a)
+
 
 
 -- Access Tables
 
+
 type RunnerType
-  = Neutral
-  | Aerobic
-  | Speed
+    = Neutral
+    | Aerobic
+    | Speed
 
 
-lookup : RunnerType -> String -> Int -> Result String (RunnerType, Int)
+lookup : RunnerType -> String -> Int -> Result String ( RunnerType, Int )
 lookup runnerType distance seconds =
-  Dict.get distance (equivalentRaceTimesTable runnerType)
-    |> Result.fromMaybe ("Invalid distance: " ++ distance)
-    |> Result.andThen (Array.map timeStrToSeconds >> Ok)
-    |> Result.andThen (Array.foldr (Result.map2 (::)) (Ok []))
-    |> Result.andThen (List.filter (\n -> n > seconds) >> Ok)
-    |> Result.andThen (List.length >> Ok)
-    |> Result.andThen (\l -> if l == 61 then Err "That time is too fast!" else if l == 0 then Err "That time is too slow!" else Ok (runnerType, l))
+    Dict.get distance (equivalentRaceTimesTable runnerType)
+        |> Result.fromMaybe ("Invalid distance: " ++ distance)
+        |> Result.andThen (Array.map timeStrToSeconds >> Ok)
+        |> Result.andThen (Array.foldr (Result.map2 (::)) (Ok []))
+        |> Result.andThen (List.filter (\n -> n > seconds) >> Ok)
+        |> Result.andThen (List.length >> Ok)
+        |> Result.andThen
+            (\l ->
+                if l == 61 then
+                    Err "That time is too fast!"
+
+                else if l == 0 then
+                    Err "That time is too slow!"
+
+                else
+                    Ok ( runnerType, l )
+            )
 
 
-equivalentRaceTimes : (RunnerType, Int) -> Result String (List (String, String))
-equivalentRaceTimes (runnerType, level) =
-  distanceList
-    |> List.map (\d -> (d, Dict.get d (equivalentRaceTimesTable runnerType) |> Maybe.withDefault Array.empty))
-    |> List.map (\(k, v) -> (k, Array.get level v |> Result.fromMaybe "out of range"))
-    |> List.foldr (\(k, v) b ->
-      b |> Result.andThen (\l ->
-        v |> Result.andThen (\i ->
-          Ok ((k, i) :: l)
-        )
-      )
-    ) (Ok [])
+equivalentRaceTimes : ( RunnerType, Int ) -> Result String (List ( String, String ))
+equivalentRaceTimes ( runnerType, level ) =
+    distanceList
+        |> List.map (\d -> ( d, Dict.get d (equivalentRaceTimesTable runnerType) |> Maybe.withDefault Array.empty ))
+        |> List.map (\( k, v ) -> ( k, Array.get level v |> Result.fromMaybe "out of range" ))
+        |> List.foldr
+            (\( k, v ) b ->
+                b
+                    |> Result.andThen
+                        (\l ->
+                            v
+                                |> Result.andThen
+                                    (\i ->
+                                        Ok (( k, i ) :: l)
+                                    )
+                        )
+            )
+            (Ok [])
 
 
-trainingPaces : (RunnerType, Int) -> Result String (List (String, (String, String)))
-trainingPaces (runnerType, level) =
-  let
-    res = Array.get (level - 1) (trainingPacesTable runnerType)
-  in
+trainingPaces : ( RunnerType, Int ) -> Result String (List ( String, ( String, String ) ))
+trainingPaces ( runnerType, level ) =
+    let
+        res =
+            Array.get (level - 1) (trainingPacesTable runnerType)
+    in
     case res of
         Just arr ->
-          Ok (Array.toList arr |> List.map2 (\x y -> Tuple.pair x y) paceList)
+            Ok (Array.toList arr |> List.map2 (\x y -> Tuple.pair x y) paceList)
 
         Nothing ->
-          Err "out of range"
+            Err "out of range"
+
 
 
 -- Utility functions
 
+
 runnerTypeFromString : String -> RunnerType
 runnerTypeFromString str =
-  case str of
-      "Aerobic" ->
-        Aerobic
+    case str of
+        "Aerobic" ->
+            Aerobic
 
-      "Speed" ->
-        Speed
+        "Speed" ->
+            Speed
 
-      _ ->
-        Neutral
+        _ ->
+            Neutral
 
 
-toTuple : List a -> Maybe (a, a)
+toTuple : List a -> Maybe ( a, a )
 toTuple l =
-  case l of
-      [a, b] ->
-        Just (a, b)
+    case l of
+        [ a, b ] ->
+            Just ( a, b )
 
-      _ ->
-        Nothing
+        _ ->
+            Nothing
 
 
 timeToSeconds : Int -> Int -> Int -> Int
 timeToSeconds hours minutes seconds =
-  (hours * 60 * 60)
-    + (minutes * 60)
-    + seconds
+    (hours * 60 * 60)
+        + (minutes * 60)
+        + seconds
 
 
 timeStrToSeconds : String -> Result String Int
 timeStrToSeconds str =
-  let
-    times = timeStrToHrsMinsSecs str
-  in
+    let
+        times =
+            timeStrToHrsMinsSecs str
+    in
     case times of
-      [hours, minutes, seconds] ->
-        Ok (timeToSeconds hours minutes seconds)
+        [ hours, minutes, seconds ] ->
+            Ok (timeToSeconds hours minutes seconds)
 
-      _ ->
-        Err ("Invalid time: " ++ str)
+        _ ->
+            Err ("Invalid time: " ++ str)
 
 
 timeStrToHrsMinsSecs : String -> List Int
 timeStrToHrsMinsSecs str =
-  String.split ":" str
-    |> List.map (String.toInt >> Maybe.withDefault 0)
+    String.split ":" str
+        |> List.map (String.toInt >> Maybe.withDefault 0)
 
 
 stripTimeStr : String -> String
 stripTimeStr str =
-  case timeStrToHrsMinsSecs str of
-    [0, min, sec] ->
-      String.fromInt min ++ ":" ++
-        case compare sec 10 of
-          LT ->
-            "0" ++ String.fromInt sec
+    case timeStrToHrsMinsSecs str of
+        [ 0, min, sec ] ->
+            String.fromInt min
+                ++ ":"
+                ++ (case compare sec 10 of
+                        LT ->
+                            "0" ++ String.fromInt sec
 
-          _ ->
-            String.fromInt sec
+                        _ ->
+                            String.fromInt sec
+                   )
 
-    _ ->
-      str
+        _ ->
+            str
--- src/Main.elm
+++ src/Main.elm
@@ -1,30 +1,34 @@
+module Main exposing (Model, Msg(..), distanceListItem, init, main, menuItem, stepItem, timeInput, trainingPaceListItem, update, updateLevel, view, viewEquivalentRaceTimes, viewLevel, viewTrainingPaces)
+
 import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Html.Events exposing (onInput, onClick)
+import Html.Events exposing (onClick, onInput)
 import MPRLevel exposing (..)
 
 
 main =
-  Browser.sandbox { init = init, update = update, view = view }
+    Browser.sandbox { init = init, update = update, view = view }
+
 
 
 -- MODEL
 
 
 type alias Model =
-  { runnerType : RunnerType
-  , distance : String
-  , hours : Maybe Int
-  , minutes : Maybe Int
-  , seconds : Maybe Int
-  , level : (Result String (RunnerType, Int))
-  }
+    { runnerType : RunnerType
+    , distance : String
+    , hours : Maybe Int
+    , minutes : Maybe Int
+    , seconds : Maybe Int
+    , level : Result String ( RunnerType, Int )
+    }
 
 
 init : Model
 init =
-  Model Neutral "5k" Nothing Nothing Nothing (Err "Enter a recent race time and distance")
+    Model Neutral "5k" Nothing Nothing Nothing (Err "Enter a recent race time and distance")
+
 
 
 -- UPDATE
@@ -41,43 +45,44 @@ type Msg
 
 update : Msg -> Model -> Model
 update msg model =
-  let
-    newModel =
-      case msg of
-        RunnerType rt ->
-          { model | runnerType = rt }
+    let
+        newModel =
+            case msg of
+                RunnerType rt ->
+                    { model | runnerType = rt }
 
-        Distance dist ->
-          { model | distance = dist }
+                Distance dist ->
+                    { model | distance = dist }
 
-        Hours hrs ->
-          { model | hours = Just hrs }
+                Hours hrs ->
+                    { model | hours = Just hrs }
 
-        Minutes mins ->
-          { model | minutes = Just mins }
+                Minutes mins ->
+                    { model | minutes = Just mins }
 
-        Seconds secs ->
-          { model | seconds = Just secs }
+                Seconds secs ->
+                    { model | seconds = Just secs }
 
-        Race dist time ->
-          case timeStrToHrsMinsSecs time of
-            [hrs, mins, secs]   ->
-              { model | hours = Just hrs, minutes = Just mins, seconds = Just secs, distance = dist }
+                Race dist time ->
+                    case timeStrToHrsMinsSecs time of
+                        [ hrs, mins, secs ] ->
+                            { model | hours = Just hrs, minutes = Just mins, seconds = Just secs, distance = dist }
 
-            _ ->
-              { model | distance = dist }
-
-  in
+                        _ ->
+                            { model | distance = dist }
+    in
     { newModel | level = updateLevel newModel }
 
 
-updateLevel : Model -> Result String (RunnerType, Int)
+updateLevel : Model -> Result String ( RunnerType, Int )
 updateLevel model =
-  if [model.hours, model.minutes, model.seconds] |> List.any (\v -> v /= Nothing) then
-    timeToSeconds (Maybe.withDefault 0 model.hours) (Maybe.withDefault 0 model.minutes) (Maybe.withDefault 0 model.seconds)
-      |> lookup model.runnerType model.distance
-  else
-    Err "Enter a recent race time and distance"
+    if [ model.hours, model.minutes, model.seconds ] |> List.any (\v -> v /= Nothing) then
+        timeToSeconds (Maybe.withDefault 0 model.hours) (Maybe.withDefault 0 model.minutes) (Maybe.withDefault 0 model.seconds)
+            |> lookup model.runnerType model.distance
+
+    else
+        Err "Enter a recent race time and distance"
+
 
 
 -- VIEW
@@ -85,141 +90,164 @@ updateLevel model =
 
 view : Model -> Html Msg
 view model =
-  div []
-    [ Html.node "link" [ Html.Attributes.rel "stylesheet", Html.Attributes.href "https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css" ] []
-    , Html.node "meta" [ Html.Attributes.name "viewport", Html.Attributes.attribute "content" "width=device-width, initial-scale=1.0" ] []
-    , div [ class "ui container" ]
-      [ h1 [ class "ui center aligned header", style "padding" "30px 0px" ]
-        [ div [ class "content" ] [ text "Maximum Performance Running Calculator" ]
+    div []
+        [ Html.node "link" [ Html.Attributes.rel "stylesheet", Html.Attributes.href "https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css" ] []
+        , Html.node "meta" [ Html.Attributes.name "viewport", Html.Attributes.attribute "content" "width=device-width, initial-scale=1.0" ] []
+        , div [ class "ui container" ]
+            [ h1 [ class "ui center aligned header", style "padding" "30px 0px" ]
+                [ div [ class "content" ] [ text "Maximum Performance Running Calculator" ]
+                ]
+            , div [ class "ui stackable three column relaxed grid" ]
+                [ div [ class "column" ]
+                    [ h3 [ class "ui dividing header" ]
+                        [ text "Runner Type"
+                        , div [ class "sub header" ] [ text "Choose a category based on how you perform against your peers" ]
+                        ]
+                    , div [ class "ui fluid vertical menu" ]
+                        [ menuItem RunnerType Neutral model.runnerType "Neutral Runner" "You fair roughly equally as well over most races distances from 5k to marathon. If you are not sure what type of runner you are use this category."
+                        , menuItem RunnerType Aerobic model.runnerType "Aerobic Monster" "You out perform your peers over the longer races but struggle in the shorter distances. Represents 10-15% of all runners."
+                        , menuItem RunnerType Speed model.runnerType "Speed Demon" "You out perform your peers in the short distances but struggle in the longer races. Represents 10-15% of all runners."
+                        ]
+                    ]
+                , div [ class "column ui form" ]
+                    [ h3 [ class "ui dividing header" ]
+                        [ text "Recent Race"
+                        , div [ class "sub header" ] [ text "Enter your time for a recent race and see equivalent times for other distances" ]
+                        ]
+                    , div [ class "three fields" ]
+                        [ timeInput "Hours" Hours model.hours
+                        , timeInput "Minutes" Minutes model.minutes
+                        , timeInput "Seconds" Seconds model.seconds
+                        ]
+                    , viewEquivalentRaceTimes model.distance model.level
+                    ]
+                , div [ class "column" ]
+                    [ h3 [ class "ui dividing header" ]
+                        [ text "Training Paces"
+                        , div [ class "sub header" ] [ text "View your level (0 - 60) and recommended paces for different workout intensities" ]
+                        ]
+                    , div [] [ viewLevel model.level ]
+                    , viewTrainingPaces model.level
+                    ]
+                ]
+            ]
         ]
-      , div [ class "ui stackable three column relaxed grid" ]
-        [ div [ class "column" ]
-          [ h3 [ class "ui dividing header" ]
-            [ text "Runner Type"
-            , div [ class "sub header" ] [ text "Choose a category based on how you perform against your peers" ]
-            ]
-          , div [ class "ui fluid vertical menu" ]
-            [ menuItem RunnerType Neutral model.runnerType "Neutral Runner" "You fair roughly equally as well over most races distances from 5k to marathon. If you are not sure what type of runner you are use this category."
-            , menuItem RunnerType Aerobic model.runnerType "Aerobic Monster" "You out perform your peers over the longer races but struggle in the shorter distances. Represents 10-15% of all runners."
-            , menuItem RunnerType Speed model.runnerType "Speed Demon" "You out perform your peers in the short distances but struggle in the longer races. Represents 10-15% of all runners."
-            ]
-          ]
-        , div [ class "column ui form" ]
-          [ h3 [ class "ui dividing header" ]
-            [ text "Recent Race"
-            , div [ class "sub header" ] [ text "Enter your time for a recent race and see equivalent times for other distances" ]
-            ]
-          , div [ class "three fields" ]
-            [ timeInput "Hours" Hours model.hours
-            , timeInput "Minutes" Minutes model.minutes
-            , timeInput "Seconds" Seconds model.seconds
-            ]
-          , viewEquivalentRaceTimes model.distance model.level
-          ]
-        , div [ class "column" ]
-          [ h3 [ class "ui dividing header" ]
-            [ text "Training Paces"
-            , div [ class "sub header" ] [ text "View your level (0 - 60) and recommended paces for different workout intensities" ]
-            ]
-          , div [ ] [ viewLevel model.level ]
-          , viewTrainingPaces model.level
-          ]
-        ]
-      ]
-    ]
 
 
 stepItem : String -> String -> Html msg
 stepItem title description =
-  div [ class "step" ]
-    [ div [ class "title" ] [ text title ]
-    , div [ class "description" ] [ text description ]
-    ]
+    div [ class "step" ]
+        [ div [ class "title" ] [ text title ]
+        , div [ class "description" ] [ text description ]
+        ]
 
 
 menuItem : (a -> Msg) -> a -> a -> String -> String -> Html Msg
 menuItem onClickMsg activatesValue modelValue textValue textDescription =
-  a [ class <| "item" ++ (if modelValue == activatesValue then " active" else ""), onClick (onClickMsg activatesValue) ]
-    [ h4 [ class "ui header" ] [ text textValue ]
-    , p [] [ text textDescription ]
-    ]
+    a
+        [ class <|
+            "item"
+                ++ (if modelValue == activatesValue then
+                        " active"
+
+                    else
+                        ""
+                   )
+        , onClick (onClickMsg activatesValue)
+        ]
+        [ h4 [ class "ui header" ] [ text textValue ]
+        , p [] [ text textDescription ]
+        ]
 
 
 timeInput : String -> (Int -> Msg) -> Maybe Int -> Html Msg
 timeInput labelText msg modelValue =
-  let
-      inputText =
-        case modelValue of
-          Just val ->
-            String.fromInt val
+    let
+        inputText =
+            case modelValue of
+                Just val ->
+                    String.fromInt val
 
-          _ ->
-            ""
-  in
+                _ ->
+                    ""
+    in
     div [ class "field" ]
-      [ label [] [ text labelText ]
-      , input [ onInput (String.toInt >> Maybe.withDefault 0 >> msg), type_ "number", Html.Attributes.min "0", value inputText ] [ ]
-      ]
+        [ label [] [ text labelText ]
+        , input [ onInput (String.toInt >> Maybe.withDefault 0 >> msg), type_ "number", Html.Attributes.min "0", value inputText ] []
+        ]
 
 
-viewEquivalentRaceTimes : String -> Result String (RunnerType, Int) -> Html Msg
+viewEquivalentRaceTimes : String -> Result String ( RunnerType, Int ) -> Html Msg
 viewEquivalentRaceTimes modelDistance level =
-  let
-    timesList = level |> Result.andThen equivalentRaceTimes
-  in
+    let
+        timesList =
+            level |> Result.andThen equivalentRaceTimes
+    in
     case timesList of
-      Ok list ->
-        div [ class "ui selection list" ] (list |> List.map (\(d, time) -> distanceListItem d modelDistance (Just time)))
+        Ok list ->
+            div [ class "ui selection list" ] (list |> List.map (\( d, time ) -> distanceListItem d modelDistance (Just time)))
 
-      Err error ->
-        div [ class "ui selection list" ] (MPRLevel.distanceList |> List.map (\d -> distanceListItem d modelDistance Nothing))
+        Err error ->
+            div [ class "ui selection list" ] (MPRLevel.distanceList |> List.map (\d -> distanceListItem d modelDistance Nothing))
 
 
 distanceListItem : String -> String -> Maybe String -> Html Msg
 distanceListItem distance modelDistance timeStr =
-  div [ class <| "item" ++ (if distance == modelDistance then " active" else ""), onClick (Race distance (Maybe.withDefault "" timeStr)) ]
-    [ div [ class "right floated content" ] [ div [class "description"] [ timeStr |> Maybe.withDefault "" |> stripTimeStr |> text ] ]
-    , a [ class "content" ] [ div [class "header"] [ text distance ] ]
-    ]
+    div
+        [ class <|
+            "item"
+                ++ (if distance == modelDistance then
+                        " active"
+
+                    else
+                        ""
+                   )
+        , onClick (Race distance (Maybe.withDefault "" timeStr))
+        ]
+        [ div [ class "right floated content" ] [ div [ class "description" ] [ timeStr |> Maybe.withDefault "" |> stripTimeStr |> text ] ]
+        , a [ class "content" ] [ div [ class "header" ] [ text distance ] ]
+        ]
 
 
-viewLevel : Result String (RunnerType, Int) -> Html Msg
+viewLevel : Result String ( RunnerType, Int ) -> Html Msg
 viewLevel level =
-  case level of
-    Ok (rt, number) ->
-      div [ class "ui info message"  ]
-        [ text <| "Level " ++ String.fromInt number ]
+    case level of
+        Ok ( rt, number ) ->
+            div [ class "ui info message" ]
+                [ text <| "Level " ++ String.fromInt number ]
 
-    Err error ->
-      div [ class "ui warning message" ] [ text error ]
+        Err error ->
+            div [ class "ui warning message" ] [ text error ]
 
 
-viewTrainingPaces : Result String (RunnerType, Int) -> Html Msg
+viewTrainingPaces : Result String ( RunnerType, Int ) -> Html Msg
 viewTrainingPaces level =
-  let
-    pacesList = level |> Result.andThen trainingPaces
-  in
+    let
+        pacesList =
+            level |> Result.andThen trainingPaces
+    in
     case pacesList of
-      Ok list ->
-        div [ class "ui relaxed list" ]
-          (list |> List.map (\(pace, range) -> trainingPaceListItem pace (Just range)))
+        Ok list ->
+            div [ class "ui relaxed list" ]
+                (list |> List.map (\( pace, range ) -> trainingPaceListItem pace (Just range)))
 
-      Err error ->
-        div [ class "ui relaxed list" ]
-          (MPRLevel.paceList |> List.map (\pace -> trainingPaceListItem pace Nothing))
+        Err error ->
+            div [ class "ui relaxed list" ]
+                (MPRLevel.paceList |> List.map (\pace -> trainingPaceListItem pace Nothing))
 
 
-trainingPaceListItem : String -> Maybe (String, String) -> Html msg
+trainingPaceListItem : String -> Maybe ( String, String ) -> Html msg
 trainingPaceListItem paceName paces =
-  let
-    paceDescription = case paces of
-      Just (minPace, maxPace) ->
-        (stripTimeStr minPace) ++ " - " ++ (stripTimeStr maxPace)
+    let
+        paceDescription =
+            case paces of
+                Just ( minPace, maxPace ) ->
+                    stripTimeStr minPace ++ " - " ++ stripTimeStr maxPace
 
-      Nothing ->
-        ""
-  in
+                Nothing ->
+                    ""
+    in
     div [ class "item" ]
-      [ div [ class "right floated content" ] [ div [ class "description" ] [ text paceDescription ] ]
-      , div [ class "content" ] [ div [ class "header" ] [ text paceName ] ]
-      ]
+        [ div [ class "right floated content" ] [ div [ class "description" ] [ text paceDescription ] ]
+        , div [ class "content" ] [ div [ class "header" ] [ text paceName ] ]
+        ]
